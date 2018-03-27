e93e12f5ef2fcd57adb6aac90ac73c4170eb075d
OzieDeviL <dlaughlinwebdev@gmail.com>
2018-03-26 19:11:34 -0700

    templates

---
+++ TemplatePages/PageGenerator/ncc-page-generator.js
@@ -0,0 +1,45 @@
+//pass the filename of the JSON scheme as the first argument in the command line after "node page-component-generator.js"
+//pass the file to emit as the subsequent argument
+let ejs = require('ejs');
+let fs = require('fs');
+let optionsFile = process.argv[2];
+let options = require(optionsFile);
+let outputFile = process.argv[3];
+
+if (Object.keys(options.article.sections) && Object.keys(options.article.sections).length > 0) {
+    let _ejs = ejs;
+    let sections = options.article.sections;
+    Object.keys(sections).forEach(function (sectionKey) {      
+        let subsectionKeys = Object.keys(sections[sectionKey].subsections);
+        if (subsectionKeys && subsectionKeys.length > 0) {            
+            subsectionKeys.forEach(function (subsectionKey) {
+                let subsection = sections[sectionKey].subsections[subsectionKey];
+                ejs.renderFile('./subsection-template.ejs', { subsection: subsection }, function (err, renderedSubsection) {
+                    if (err) throw new Error(err);
+                    subsection.body = renderedSubsection;
+                });
+            });
+        }
+        ejs.renderFile('./section-template.ejs', { section: sections[sectionKey] }, function (err, renderedSection) {
+            if (err) throw new Error(err);
+            sections[sectionKey].body = renderedSection;
+        });
+    });
+}
+
+ejs.renderFile(
+    './article-template.ejs'
+    , { article: options.article }
+    , null //options
+    , function (onError, renderedString) {
+        if (onError) {
+            throw new Error(onError);
+        }
+        else {
+            fs.appendFile(outputFile, renderedString, (err) => {
+                if (err) throw err;
+                console.log('The "data to append" was appended to file!');
+            })
+        }
+    }
+);
